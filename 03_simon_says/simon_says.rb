a52c084bfc765eb6a817f969891ea313d2f97db2
margaretyjkim <margaret.yj.kim@gmail.com>
2015-11-02 17:24:58 -0500

    started the assignment number 4 and created pig_latin.rb and created translate method

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -6,22 +6,57 @@ def shout( str )
 	str.upcase
 end
 
-def repeat( string, num = 1 )
-	if num >= 3
-		"#{string} #{string} #{string}"
-	else
-		"#{string} #{string}"
+def repeat( string, num = 2 )
+	# if num >= 3
+	# 	"#{string} #{string} #{string}"
+	# else
+	# 	"#{string} #{string}"
+	#
+	repeated_string = []
+
+	num.times do |n|
+		repeated_string << string
 	end
+
+		repeated_string.join(' ')
 end
 
 def start_of_word ( word, number )
+	word[0, number]
+
+
+	 # letters = word.split("")
+	 # counter = 0
+	 # to_keep = []
+
+	 # letters.each do |character|
+
+		#  	if counter < number #keep going!
+		#  		to_keep << character
+		#  	else #time to stop!
+		#  		return to_keep.join
+		#  	end
+
+		#  	counter += 1
+
+	 # end
 
 end
 
-def first_word
+def first_word(phrase)
+	phrase.split[0]
 
 end
 
-def titleize
-
+def titleize(title)
+	lower_case = ["and", "over", "the"]
+	capitalized = title.split.map do |word|
+		if lower_case.include? word
+			word
+		else
+			word.capitalize
+		end
+	end
+	capitalized[0].capitalize!
+	capitalized.join(' ')
 end
