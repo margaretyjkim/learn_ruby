62710203f97d4d9d53c464c21a134ba97b4f7da3
margaretyjkim <margaret.yj.kim@gmail.com>
2015-11-08 17:16:00 -0500

    did it again for the second time and now the qu rule works as well

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,3 +1,37 @@
 def translate(str)
-	"appleay"
+	words = str.split
+	words.map! do |word|
+		translate_word(word)
+	end
+	words.join(" ")
 end
+
+def translate_word(word)
+	first_letter = word[0]
+	second_letter = word[1]
+	third_letter = word[2]
+	vowels = ["a", "e", "i", "o", "u"]
+	if vowels.include?(first_letter)
+		word + "ay"
+	else
+		if vowels.include?(second_letter)
+				if first_letter == "q" && second_letter == "u"
+					word[2..-1] + first_letter + second_letter + "ay"
+				else
+			 		word[1..-1] + first_letter + "ay"
+				end
+		else
+			if vowels.include?(third_letter)
+				if second_letter == "q" && third_letter == "u"
+					word[3..-1] + "squay"
+				else
+					word[2..-1] + first_letter + second_letter + "ay"
+				end
+			else
+				word[3..-1] + first_letter + second_letter + third_letter + "ay"
+			end
+		end
+	end
+end
+
+
